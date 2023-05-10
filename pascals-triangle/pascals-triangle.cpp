d6ad03ed149c6a570319a335d33d20eebf9b4da3
Deep Akabari <117529824+Hustler-Deep@users.noreply.github.com>
2023-05-10 19:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (50.22%) - LeetSync

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
+    const int mod = 1e9 + 7;
 public:
-    int calculateNcR(int n, int r){
-        long long res = 1;
-        for(int i=0; i<r; i++){
-            res = (res * (n-i)) / (i+1);
-        }
-        return (int)res;
+    vector<int> nthRowOfPascalTriangle(int n) {
+        vector<int> ansRow;
+        for(int i = 0; i < n; i++){
+            ansRow.push_back(1);
+            for(int j = i-1; j>=1; j--){
+                ansRow[j] = (ansRow[j] + ansRow[j-1]) % mod;
+            }
+        }    
+        return ansRow;
     }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int row = 1; row<=numRows; row++){
-            vector<int> tempList;
-            for(int col=1; col<=row; col++){
-                tempList.push_back(calculateNcR(row-1, col-1));
-            }
-            ans.push_back(tempList);
+        for(int row=1; row<=numRows; row++){
+            ans.push_back(nthRowOfPascalTriangle(row));
         }
         return ans;
     }
